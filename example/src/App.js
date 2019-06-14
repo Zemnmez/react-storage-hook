e452a5e1e5f964feccd85cfa0d9585bc2040a22d
tshadwel <tshadwel@amazon.com>
2019-06-14 18:44:59 +0100

    working hook!

--- example/src/App.js
+++ example/src/App.js
@@ -1,27 +1,52 @@
-import React, { PureComponent, useState, createRef } from 'react'
+import React from 'react'
 
-import Storage from 'react-storage'
+import { useStorage } from 'react-storage'
 
 export default class App extends React.PureComponent {
   render () {
     return (
-      <SavedCheckbox/>
+      <div>
+        <p/>Any input entered here will be saved, and
+        propagated to any other open instances of this page.
+        <p/><SavedCheckbox/>
+        <p/><SavedTextarea/>
+        <p/>Here's another instance of this page.
+        There's no direct communication between these instances.
+        <p/><SelfFrame/>
+      </div>
     )
   }
 }
 
+const SelfFrame = () => {
+  if (window.location.search=="?framed") return null;
+  return <iframe src="?framed"/>
+}
+
 const SavedCheckbox = () => {
-  const [checked, setChecked] = React.useState(false);
-  const checkRef = React.createRef();
-  const onCheckChange = () => setChecked(checkRef.current.value);
+  const [checked, setChecked] = useStorage('saved-checkbox-checked', {
+    placeholder: false
+  });
 
-  return <Storage value={{checked}} name={"checkbox-checked"}>
-      {({checked: storedChecked }) => <input
-        type="checkbox"
-        checked={storedChecked}
-        ref={checkRef}
-        onChanged={onCheckChange}
-      />}
-    </Storage>
+  const onChange = e => setChecked(e.target.checked);
 
+  return <input {...{
+    checked,
+    onChange,
+    type: "checkbox"
+  }}/>
+
+}
+
+const SavedTextarea = () => {
+  const [text, setText] = useStorage('saved-text', {
+    placeholder: ""
+  });
+
+  const onChange = e => setText(e.target.value);
+
+  return <textarea {...{
+    onChange,
+    value: text
+  }}/>
 }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,31 @@
-import React from 'react'
-import PropTypes from 'prop-types'
 import { fromJS } from 'immutable'
+import React from 'react'
 const Immutable = { fromJS }
 
-const Storage = ({
-  value,
-  name,
-  storageArea = window.localStorage,
-  children
-}) => {
-  const [localValue, setLocalValue] = React.useState(storageArea.getItem(name) || value)
+export const useStorage = (name, {
+  placeholder, storageArea = window.localStorage } = {}) => {
+  const [value, setValue] = React.useState(Immutable.fromJS(JSON.parse(storageArea.getItem(name))) || placeholder)
 
   const onStorage = ({ key, oldValue, newValue, storageArea: eventStorageArea }) => {
     // if it's not our record, the value has not changed, or it's for another storage area we skip
     // there could, or perhaps *should* be an extra check for if our local value differs from
     // the stored value, but this should be handled internally via React.
     if (key !== name || oldValue === newValue || storageArea !== eventStorageArea) return
-    return setLocalValue(Immutable.fromJS(JSON.parse(newValue)))
+    return setValue(Immutable.fromJS(JSON.parse(newValue)))
+  }
+
+  const setStorage = (value) => {
+    const oldValue = storageArea.getItem(name)
+    const newValue = JSON.stringify(value)
+    storageArea.setItem(name, newValue)
+    // fire our own onStorage event
+    // because onStorage only fires between windows
+    onStorage({
+      key: name,
+      newValue,
+      oldValue,
+      storageArea
+    })
   }
 
   // listen to storage events on mount and unmount
@@ -27,24 +36,5 @@ const Storage = ({
     }
   }, [])
 
-  // sync any changes in value ... NB: *NOT* a change in `localValue`
-  // this is simply when the prop `value` changes. localValue
-  // is only set by changing what's in storage.
-  React.useEffect(() => {
-    storageArea.setItem(name, JSON.stringify(value))
-  }, [value])
-
-  return children(localValue)
+  return [value, setStorage]
 }
-
-Storage.propTypes = {
-  value: PropTypes.object,
-  name: PropTypes.string.isRequired,
-  storageArea: PropTypes.shape({
-    getItem: PropTypes.func.isRequired,
-    setItem: PropTypes.func.isRequired
-  }),
-  children: PropTypes.func.isRequired
-}
-
-export default Storage
