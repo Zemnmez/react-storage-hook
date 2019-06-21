db3d95c105b854ef04538da2db7508df0d211f8d
tshadwel <tshadwel@amazon.com>
2019-06-21 21:57:00 +0100

    it worked... wow

--- src/index.tsx
+++ src/index.tsx
@@ -2,23 +2,13 @@ import { fromJS } from 'immutable'
 import * as React from 'react'
 const Immutable = { fromJS }
 
-/**
- * @hidden
- */
-interface SimpleStorageEvent {
-  readonly key: string | null
-  readonly newValue: string | null
-  readonly oldValue: string | null
-  readonly storageArea: Storage | null
-}
-
-type value = any
-type setValue<Kind> = (newValue: Kind) => void
+type stored = any
+type setStored<Kind> = (newValue: Kind) => void
 
 export const useStorage = <InputType extends {}>(name: string, {
   placeholder, storageArea = window.localStorage } : {
     placeholder: any, storageArea: Storage
-  }): [value, setValue<InputType>] => {
+  }): [stored, setStored<InputType>] => {
     const currentItem = storageArea.getItem(name)
     const [value, setValue] = React.useState(
       currentItem?
@@ -26,8 +16,8 @@ export const useStorage = <InputType extends {}>(name: string, {
       placeholder
     )
 
-  const onStorage = (event: SimpleStorageEvent) => {
-    const { key, oldValue, newValue, storageArea: eventStorageArea } = event
+    const onStorage = ({ key, oldValue, newValue, storageArea: eventStorageArea }:
+                       Pick<StorageEvent, 'key' | 'oldValue' | 'newValue' | 'storageArea'>) => {
     // if it's not our record, the value has not changed, or it's for another storage area we skip
     // there could, or perhaps *should* be an extra check for if our local value differs from
     // the stored value, but this should be handled internally via React.
